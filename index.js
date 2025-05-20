58996ce6159c30bca18f186bfd80c03f4bd6302f
Keithkat64 <keithkat64@gmail.com>
2025-05-20 21:25:16 +1000

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`1618Stayman app listening at http://localhost:${port}`);
+});
